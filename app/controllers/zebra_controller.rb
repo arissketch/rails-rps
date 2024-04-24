e5e5e43827455fb3f8c32f59fa7aae29b1b45d7c
arissketch <60331491+arissketch@users.noreply.github.com>
2024-04-24 16:14:15 +0000

    play

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -5,17 +5,43 @@ class ZebraController < ApplicationController
   end
 
 def giraffe
+  @my_move="rock"
   @random_move=["rock","paper","scissors"].sample
+  if @random_move=="rock"
+    @result="tied"
+  elsif @random_move=="paper"
+    @result="lost"
+  else
+    @result="won"
+  end
   render({:template => "game_templates/play_rock"})
 end
 
 def elephant
+  @my_move="paper"
   @random_move=["rock","paper","scissors"].sample
+  if @random_move=="rock"
+    @result="won"
+  elsif @random_move=="paper"
+    @result="tied"
+  else
+    @result="lost"
+  end
   render({:template => "game_templates/play_rock"})
 end
 
 def lion
+
+  @my_move="scissors"
   @random_move=["rock","paper","scissors"].sample
+  if @random_move=="rock"
+    @result="lost"
+  elsif @random_move=="paper"
+    @result="won"
+  else
+    @result="tied"
+  end
+
   render({:template => "game_templates/play_rock"})
 end
 
